4ee3f28d588857caa16f67be7943238c9d85970c
Aryan Desai <73512789+elatedleo23409@users.noreply.github.com>
2022-04-13 16:19:00 +0530

    Update settings.py

--- hospital_mgmt_sys/settings.py
+++ hospital_mgmt_sys/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+&p7ero-agmf007x!0t$vxmdzidp6x+8p$ptdvy-%tv+=x-_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','hospitalmgmtsys.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
